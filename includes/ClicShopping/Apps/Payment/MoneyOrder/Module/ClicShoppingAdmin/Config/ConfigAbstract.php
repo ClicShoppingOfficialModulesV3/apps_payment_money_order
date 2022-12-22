4616aa9310bd225890392d38770b23a304aed5ce
ClicShopping <loic.richard2@gmail.com>
2022-12-22 13:59:46 -0500

    update

--- includes/ClicShopping/Apps/Payment/MoneyOrder/Module/ClicShoppingAdmin/Config/ConfigAbstract.php
+++ includes/ClicShopping/Apps/Payment/MoneyOrder/Module/ClicShoppingAdmin/Config/ConfigAbstract.php
@@ -4,7 +4,7 @@
    * @copyright 2008 - https://www.clicshopping.org
    * @Brand : ClicShopping(Tm) at Inpi all right Reserved
    * @Licence GPL 2 & MIT
-   * @licence MIT - Portion of osCommerce 2.4
+
    * @Info : https://www.clicshopping.org/forum/trademark/
    *
    */
@@ -45,7 +45,7 @@
       $cut_length = \strlen('CLICSHOPPING_APP_MONEYORDER_' . $this->code . '_');
 
       foreach ($this->getParameters() as $key) {
-        $p = strtolower(substr($key, $cut_length));
+        $p = mb_strtolower(substr($key, $cut_length));
 
         $class = 'ClicShopping\Apps\Payment\MoneyOrder\Module\ClicShoppingAdmin\Config\\' . $this->code . '\Params\\' . $p;
 
@@ -99,7 +99,7 @@
       $cut_length = \strlen($cut);
 
       foreach ($this->getParameters() as $key) {
-        $p = strtolower(substr($key, $cut_length));
+        $p = mb_strtolower(substr($key, $cut_length));
 
         $class = 'ClicShopping\Apps\Payment\MoneyOrder\Module\ClicShoppingAdmin\Config\\' . $this->code . '\Params\\' . $p;
 
